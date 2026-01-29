db6e49abd97ffbaea5b2ab7ce30a82a763f7a06a
Veren Jessica <verenjessica673@gmail.com>
2026-01-30 00:27:58 +0700

    backend1

---
+++ src/aps.ts
@@ -0,0 +1,11 @@
+import express from "express";
+import cors from "cors"
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.get("/", (req, res) => {
+    res.send("Sporton Backend API is running");
+})
+
+export default app;
