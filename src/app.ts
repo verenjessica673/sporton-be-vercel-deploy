0f6a51127b7bd9e2790945bee214bd03ce822d53
Veren Jessica <verenjessica673@gmail.com>
2026-02-03 13:20:44 +0700

    BE Auth

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors"
 import authRoutes from "./routes/auth.routes"
+import { authenticate } from "./middlewares/auth.middleware";
 
 const app = express();
 
@@ -10,6 +11,10 @@ app.use(express.json());
 app.use("/api/auth", authRoutes)
 app.get("/", (req, res) => {
     res.send("Sporton Backend API is running");
+});
+
+app.get("/test-middleware", authenticate, (req, res) => {
+  res.send("Bisa diakses dengan token");
 });
 
 export default app;
